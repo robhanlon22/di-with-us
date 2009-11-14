42d8812ed10185de1007af087983306d5d110635
David Balatero <david@bitwax.cd>
2009-11-14 15:12:42 -0800

    Implemented Twitter bullshit

---
+++ lib/di-with-us/twitter.rb
@@ -0,0 +1,31 @@
+begin
+  require 'twitter'
+rescue LoadError
+  abort "Hey jughead, sudo gem install twitter!"
+end
+
+module DiWithUs
+  class Twitter
+    def initialize
+      @config = YAML.load_file(File.dirname(__FILE__) +
+                               "/../../config/twitter.yml")
+      httpauth = ::Twitter::HTTPAuth.new(@config['username'],
+                                         @config['password'])
+      @client = ::Twitter::Base.new(httpauth)
+    end
+
+    def update_status(message)
+      @client.update(message)
+    end
+
+    # Returns nil or an array ['of', 'words']
+    def get_last_status_to_a
+      statuses = @client.user_timeline
+      if statuses.empty?
+        nil
+      else
+        statuses.first.text.strip.split(/\s+/)
+      end
+    end
+  end
+end
