9453ea1d4e1d8b1b4c2de7e8391aa77ea6f29ac0
David Balatero <david@bitwax.cd>
2009-11-14 15:38:36 -0800

    Fuck you can't you remove whitespace?!?!

--- lib/di-with-us/generator.rb
+++ lib/di-with-us/generator.rb
@@ -7,10 +7,10 @@ module DIWithUs
     THESAURUS_URL = 'http://words.bighugelabs.com/api/2'
     API_KEY = YAML.load(File.read(
                         File.join(
-                        File.dirname(__FILE__), 
-                                     '..', 
-                                     '..', 
-                                     'config', 
+                        File.dirname(__FILE__),
+                                     '..',
+                                     '..',
+                                     'config',
                                      'big-huge-thesaurus.yml')))['api-key']
 
     def self.generate
