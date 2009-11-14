1ab4713606e4eb3662833ac279eaf23425fe47a9
Rob Hanlon <robhanlon@gmail.com>
2009-11-14 15:39:53 -0800

    Fixed indentation

--- lib/di-with-us/generator.rb
+++ lib/di-with-us/generator.rb
@@ -1,17 +1,16 @@
 require 'httparty'
-require 'pp'
 
 module DIWithUs
   class Generator
     VENUE = %w[Healthy Times Fun Club]
     THESAURUS_URL = 'http://words.bighugelabs.com/api/2'
     API_KEY = YAML.load(File.read(
-                        File.join(
-                        File.dirname(__FILE__),
-                                     '..',
-                                     '..',
-                                     'config',
-                                     'big-huge-thesaurus.yml')))['api-key']
+      File.join(
+        File.dirname(__FILE__),
+        '..',
+        '..',
+        'config',
+        'big-huge-thesaurus.yml')))['api-key']
 
     def self.generate
       VENUE.map { |word|
